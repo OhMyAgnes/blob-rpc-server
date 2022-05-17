69005c2c530e28d907b6b129ab848068977edddd
unknown <1251437798@qq.com>
2022-05-17 22:12:43 +0800

    sdk-0517

--- sdk/main.cpp
+++ sdk/main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include "myclass.h"
+#include "src/myclass.h"
 #include <opencv2/opencv.hpp>
 
 
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-	cv::Mat src = cv::imread("/media/qulab/2fbfe086-740c-4d24-acff-839bf21fb2c2/zr/code/example/1.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat src = cv::imread("/media/qulab/2fbfe086-740c-4d24-acff-839bf21fb2c2/zr/code/calca/1.jpg", cv::IMREAD_GRAYSCALE);
 	ReadImage a(0, src);
 	cout << a.getSum();
 	getchar();
